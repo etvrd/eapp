a1b1e3cc33b84e1bd1e204863d9b47f1e8b03431
etvrd <atomic_boy@mail.ru>
2021-12-12 20:27:26 +0300

    2 задание

--- app/src/main/java/ru/mycomp/eapp/MainActivity.java
+++ app/src/main/java/ru/mycomp/eapp/MainActivity.java
@@ -2,13 +2,21 @@ package ru.mycomp.eapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Click(View view) {
+        Intent intent=new Intent(MainActivity.this,EditTextActivity.class);
+        startActivity(intent);
     }
 }
